df90664d6f035d9eb47fb3d4d50674bec4abc6a8
Ahmad Saiful <ahmadsaifullpro@gmail.com>
2025-07-13 13:15:25 +0700

    pembuatan halaman login fe saipul

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/login', 'auth.login')->name('login');
+
+// Route::post('login', [AuthController::class, 'proses_login'])->name('prosesLogin');
