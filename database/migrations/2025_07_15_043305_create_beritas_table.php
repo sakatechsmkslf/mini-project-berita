ce6955f1112df195068ce929b50d36979f70d52d
hexaviate <fahmi.anargya15@gmail.com>
2025-07-15 11:46:56 +0700

    membuat model dan migration Berita, Tag dan Berita Tag

---
+++ database/migrations/2025_07_15_043305_create_beritas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('beritas', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_berita');
+            $table->string('isi_berita');
+            $table->string('path_file');
+            $table->enum('status', ['published', 'pending', 'not published']);
+            $table->foreignId('berita_tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beritas');
+    }
+};
