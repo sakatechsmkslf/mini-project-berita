9f8ecd7e9531c5aa7d437f90958502907faff6b3
hexaviate <fahmi.anargya15@gmail.com>
2025-07-15 13:45:01 +0700

    membuat relationship one to many antara tabel categories dan berita. membuat relationship many to manya antara tabel berita dan tag.

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Berita extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the category that owns the Berita
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * The tag that belong to the Berita
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tag(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'berita_tag');
+    }
 }
