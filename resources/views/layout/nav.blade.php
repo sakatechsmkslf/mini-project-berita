c4d1891c9a053173423aab8eb1633fff90f57edd
iiqbaael <Muhammadiqbal5806@gmail.com>
2025-07-21 14:36:25 +0700

    revisi halaman login

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <a class="navbar-brand">SAKATECH</a>
+    <a class="navbar-brand fw-bold">SAKATECH</a>
     <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
