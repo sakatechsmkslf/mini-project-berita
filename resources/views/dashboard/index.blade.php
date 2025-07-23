6fd76e6a068149f67b43fb553a9b2f3c21076819
qmenggg <azizyaqy@gmail.com>
2025-07-23 15:08:30 +0700

    pembuatan berita untuk tampilan di halaman dahboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,9 +1,26 @@
 @extends('layout.main')
 @section('konten')
     <div class="card p-3 bg-dark">
-        <h3 class="text-white">Hai</h3>
+        <div class="row">
+            {{-- @foreach ($berita as $item)
+                <div class="col">
+                    <div class="card m-2" style="width: 15rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$item->judul}}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the
+                                card’s
+                                content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach --}}
+        </div>
+        {{-- <h3 class="text-white">Hai</h3>
         <p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis animi rem odit quam velit,
             commodi architecto earum quos eos quo! Suscipit iure, mollitia quo quae deserunt reprehenderit. Sunt, at odio!
-        </p>
+        </p> --}}
     </div>
 @endsection
