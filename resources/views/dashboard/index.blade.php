c727aac335c00bdb8a5a5cc99f759b204f6d7139
qmenggg <azizyaqy@gmail.com>
2025-07-26 15:08:33 +0700

    saya menyelesaikan tamppilan halaman dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,14 +5,16 @@
             @foreach ($berita as $item)
                 <div class="col">
                     <div class="card m-2" style="width: 15rem;">
-                        <img src="{{asset('images/'.$item->path_file)}}" class="img-thumbnail img-fluid" style="max-height: 240px; max-width: 240px; object-fit:contain " alt="Gambar Berita">
+                        <img src="{{ asset('images/' . $item->path_file) }}" class="img-thumbnail img-fluid"
+                            style="max-height: 240px; max-width: 240px; object-fit:contain " alt="Gambar Berita">
                         <div class="card-body">
-                            <h5 class="card-title">{{$item->judul_berita}}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                                the
-                                card’s
-                                content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">{{ $item->judul_berita }}</h5>
+                            <p class="card-title">{{ $item->category->nama }}</p>
+                            <sub>
+                                @foreach ($item->tag as $k)
+                                    {{ $k->nama }},
+                                @endforeach
+                            </sub>
                         </div>
                     </div>
                 </div>
